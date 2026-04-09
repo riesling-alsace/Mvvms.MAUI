36ccb67ddebc06e1c7be556b9f25e24e909e7ac2
Riesling Alsace <riesling.alsace@gmail.com>
2026-04-09 12:18:32 +0900

    INortifyPropertiesChangedの追加 Lock()の追加

--- ViewModelBase.cs
+++ ViewModelBase.cs
@@ -1,12 +1,10 @@
-﻿using System.ComponentModel;
+﻿namespace Riesling.Mvvms.MAUI;
 
-namespace Riesling.Mvvms.MAUI;
-
-public class ViewModelBase : BindableBase {
+public class ViewModelBase : PropertyBase {
 
 	#region Raise Methods
 
-	protected override void RaisePropertiesChanged(params string[] propertyNames) {
+	public override void RaisePropertiesChanged(params string[] propertyNames) {
 		if (MainThread.IsMainThread) {
 			base.RaisePropertiesChanged(propertyNames);
 		} else {
@@ -23,40 +21,34 @@ public class ViewModelBase : BindableBase {
 }
 
 public abstract class ViewModelBase<TModelBase> : ViewModelBase
-	where TModelBase : BindableBase {
+    where TModelBase : PropertyBase, new() {
 
-	#region Fields
+    #region Properties
 
-	protected TModelBase? _Model;
+    public TModelBase Model {
+        get;
+        private set;
+    }
 
-	#endregion
+    #endregion
 
-	#region Properties
-
-	public required TModelBase Model {
-		get => _Model!;
-		set => SetProperty(ref _Model, value, Model_Changed);
-	}
-
-	protected virtual void Model_Changed(TModelBase? newModel, TModelBase? oldModel) {
-		if (oldModel != null) {
-			oldModel.PropertyChanged -= Model_PropertyChanged;
-		}
-		if (newModel != null) {
-			newModel.PropertyChanged += Model_PropertyChanged;
-		}
-	}
-
-	#endregion
-
-	#region Constructors
+    #region Constructors
 
     protected ViewModelBase(TModelBase model) {
-		Model = model;
-	}
+        Model = model;
+        Model.PropertiesChanged += Model_PropertiesChanged;
+    }
 
-	protected abstract void Model_PropertyChanged(object? sender, PropertyChangedEventArgs e);
+    ~ViewModelBase() {
+        Model.PropertiesChanged -= Model_PropertiesChanged;
+    }
 
-	#endregion
+    #endregion
+
+    #region Methods
+
+    protected abstract void Model_PropertiesChanged(object? sender, PropertiesChangedEventArgs e);
+
+    #endregion
 
 }
